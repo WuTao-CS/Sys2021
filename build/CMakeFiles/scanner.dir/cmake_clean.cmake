bd74992b1637421b8775271d79e14d6ce4c8257a
WuTao-CS <747866429@qq.com>
2021-06-23 23:54:35 +0800

    修改前段不能识别const的bug

---
+++ build/CMakeFiles/scanner.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../front/sysy_parser.cc"
+  "../front/sysy_parser.hh"
+  "../front/sysy_scanner.cc"
+  "CMakeFiles/scanner.dir/front/sysy_scanner.cc.o"
+  "CMakeFiles/scanner.dir/front/sysy_scanner.cc.o.d"
+  "libscanner.a"
+  "libscanner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/scanner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sysy_driver.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/linux/Desktop/compiler/Sys2021/front/sysy_driver.cc" "CMakeFiles/sysy_driver.dir/front/sysy_driver.cc.o" "gcc" "CMakeFiles/sysy_driver.dir/front/sysy_driver.cc.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
