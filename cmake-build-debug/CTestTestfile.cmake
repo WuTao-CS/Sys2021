4176fe0149a519d42d583971192294e2a1c11e38
WuTao-CS <747866429@qq.com>
2021-06-30 11:04:21 +0800

    删除debug文件,通过所有功能测试样例

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/linux/Desktop/compiler/Sys2021_v3
-# Build directory: /home/linux/Desktop/compiler/Sys2021_v3/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
